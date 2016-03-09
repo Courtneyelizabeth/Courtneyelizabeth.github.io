3699d1874f14da2cad2ccf0823b051ab338b9c48
Courtney Elizabeth <hecklercourtney@gmail.com>
2016-03-09 10:13:28 -0700

    addedphoto descripton and steamboat aspen api current weather

--- weather.js
+++ weather.js
@@ -1,49 +1,62 @@
 var aspen = {
   "async": true,
   "crossDomain": true,
-  "url": "http://api.openweathermap.org/data/2.5/weather?zip=81611%2Cus&appid=44db6a862fba0b067b1930da0d769e98&units=imperial",
+  "url": "http://api.openweathermap.org/data/2.5/group?id=5582371,5412230&units=metric&appid=2cf669fd8a60ec333738f29801599eee",
   "method": "GET"
 }
 
 $.ajax(aspen).done(function (data) {
   console.log(data);
-	var city = data.name;
-  var description = data["weather"][0]["description"];
-  var temp1 = data["main"]["temp"];
+	var city = data["list"][1]["name"];
+  var description = data["list"][1]["weather"][0]["description"];
+	var humidity = data["list"][1]["main"]["humidity"];
+  var temp1 = data["list"][1]["main"]["temp"];
   console.log(temp1);
-  var number = temp1;
+  var number = ((temp1 * 1.8) +32);
   var temp = Math.round(number);
-	var wind1 = data["wind"]["deg"];
-	var num = wind1;
-	var wind = Math.round(num);
-  console.log(temp);
+	var windKph = data["list"][1]["wind"]["speed"];
+	/*console.log(windKph);*/
+	var windMph = (windKph /1.60934);
+	var windMph = Math.round(windMph);
 	console.log(city);
+  console.log(temp);
   console.log(description);
+	console.log(humidity);
+	console.log(windMph);
   $("#description").html(description);
   $("#city").append(city);
   $("#temp").prepend(temp);
-	$("#wind").append(wind);
+	$("#humidity").prepend(humidity);
+	$("#windMph").prepend(windMph);
 });
 
-/*var aspen = {
+var steamboat = {
   "async": true,
   "crossDomain": true,
-  "url": "http://api.openweathermap.org/data/2.5/weather?zip=81611%2Cus&appid=44db6a862fba0b067b1930da0d769e98",
+  "url": "http://api.openweathermap.org/data/2.5/group?id=5582371,5412230&units=metric&appid=2cf669fd8a60ec333738f29801599eee",
   "method": "GET"
 }
-$.ajax(aspen).done(function (data) {
+$.ajax(steamboat).done(function (data) {
   console.log(data);
-	var city = data.name;
-  var description = data["weather"][0]["description"];
-	var temp = data["main"]["temp"];
-  console.log(temp);
-  var number = parseInt(temp2);
-  var temp2 = Math.round(number);
-  console.log(temp2);
+	var city = data["list"][0]["name"];
+  var description = data["list"][0]["weather"][0]["description"];
+	var humidity2= data["list"][0]["main"]["humidity"];
+	var degree1 = data["list"][0]["main"]["temp"];
+	console.log(degree1);
+	var windSpeedKph = data["list"][0]["wind"]["speed"];
+	/*console.log(windSpeedKph);*/
+	var windSpeedMph = (windSpeedKph / 1.60934);
+	var windSpeedMph = Math.round(windSpeedMph);
+	var degree= ((degree1 * 1.8) +32);
+  var degree = Math.round(degree);
 	console.log(city);
+	console.log(degree);
   console.log(description);
+	console.log(humidity2);
+	console.log(windSpeedMph);
   $("#description2").html(description);
   $("#city2").append(city);
-	$("#temp2").prepend(temp);
-
-});*/
+	$("#degree").prepend(degree);
+	$("#humidity2").prepend(humidity2);
+	$("#windSpeedMph").prepend(windSpeedMph);
+});
